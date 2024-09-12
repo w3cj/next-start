658d51e582ab12802c80aac4df37d51bf2fd70ac
w3cj <cj@null.computer>
2024-09-12 02:05:57 -0600

    :sparkles: setup env

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import createJiti from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env/server.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
---
+++ src/env/server.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { ZodError, z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
